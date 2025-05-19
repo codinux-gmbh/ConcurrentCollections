5c1dd12c40044e55cf301b2facbef9e52a49ad8f
dankito <git@dankito.net>
2025-05-19 03:58:43 +0200

    Added scripts to publish library

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,9 @@ allprojects {
     repositories {
         mavenCentral()
     }
+
+
+    ext["sourceCodeRepositoryBaseUrl"] = "github.com/codinux-gmbh/ConcurrentCollections"
+
+    ext["projectDescription"] = "Concurrent collections and atomic references implementation for Kotlin Multiplatform"
 }
--- concurrent-collections/build.gradle.kts
+++ concurrent-collections/build.gradle.kts
@@ -91,3 +91,8 @@ kotlin {
         }
     }
 }
+
+
+if (File(projectDir, "../gradle/scripts/publish-codinux.gradle.kts").exists()) {
+    apply(from = "../gradle/scripts/publish-codinux.gradle.kts")
+}
